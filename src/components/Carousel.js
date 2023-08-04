6d2725cd153712278527f2af7b1c4958ff73798f
Enocho1-1 <enoch.okyere@gmail.com>
2023-08-04 11:21:26 -0400

    image carousel component

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,4 @@
-// import Slider from "react-slick";
+
 import ImgOne from "../assests/Img1.jpg"
 import ImgTwo from "../assests/SlideImg2.jpg"
 import ImgThree from "../assests/SlideImg3.jpg"
@@ -10,8 +10,31 @@ export const Carousel = () => {
 
   return (
 
-    <div>
-      {/* <img src={ImgOne} alt="" /> */}
+    <div id="carouselExampleIndicators" className="carousel slide">
+      <div className="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={ImgTwo} className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src={ImgThree} className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src={ImgFour} className="d-block w-100" alt="..." />
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
 
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Logo from "../assests/cube.png"
 
 
 export const Header = () => {
-  const [isHidden, setIsHidden] = useState(true)
+  const [isHidden, setIsHidden] = useState(false)
   return (
     
     <nav className="bg-white border-gray-200">
@@ -13,14 +13,14 @@ export const Header = () => {
             <img src={Logo} className="h-8 mr-3" alt="Rudiments Logo" />
             <span className="self-center text-3xl leading-loose font-semibold whitespace-nowrap text-blk font-Bebas ">RUDIMENTS</span>
         </Link>
-        <button onClick={() => setIsHidden(!isHidden)} data-collapse-toggle="navbar-dropdown" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+        <button onClick={() => setIsHidden(!isHidden)} data-collapse-toggle="navbar-dropdown" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-dropdown" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
             </svg>
         </button>
         <div className={ isHidden ? `w-full md:block md:w-auto` :`hidden w-full md:block md:w-auto`} id="navbar-dropdown">
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4  rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
             <li>
               <NavLink to="/shop" className="block py-2 pl-3 pr-4 text-xl font-Bebas text-blk rounded hover:bg-gray-100">Shop</NavLink>
             </li>
