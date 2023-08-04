88181a76784152f31811625dec9ddaf14bfcad45
Enocho1-1 <enoch.okyere@gmail.com>
2023-08-04 11:35:47 -0400

    carousel styling

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,6 +3,7 @@ import ImgOne from "../assests/Img1.jpg"
 import ImgTwo from "../assests/SlideImg2.jpg"
 import ImgThree from "../assests/SlideImg3.jpg"
 import ImgFour from "../assests/SlideImg4.jpg"
+import "./Carousel.css"
 
 
 
