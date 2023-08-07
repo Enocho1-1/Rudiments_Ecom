776e88c8145b68b3f94dea78d2f2f0f5bbfde4bc
Enocho1-1 <73598581+Enocho1-1@users.noreply.github.com>
2023-08-07 00:02:42 -0400

    scroll top

--- src/components/ScrollTop.js
+++ src/components/ScrollTop.js
@@ -4,9 +4,7 @@ import { useLocation } from "react-router-dom";
 export const ScrollTop = () => {
     const pathname = useLocation()  
     useEffect( () => {
-        const scrollToTop = () => window.scrollTo(0,0)
-        console.log(pathname)
-        scrollToTop()
+      window.scrollTo(100,20)
     },[pathname])
 
   return null
