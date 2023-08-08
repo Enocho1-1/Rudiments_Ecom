8b3645c0cfccbb5bf72971e94730a419771d78eb
Enocho1-1 <enoch.okyere@gmail.com>
2023-08-08 09:47:18 -0400

    pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,6 +2,9 @@
 
 export const Pagination = ({totalAmount,postsPerPage, paginate}) => {
 
+  const scrollTop = () =>{
+    window.scrollTo({top:10, left:0});
+  }
   const pageArray = []
 
   for(let i = 1; i <= Math.ceil(totalAmount/postsPerPage); i++){
@@ -13,7 +16,7 @@ export const Pagination = ({totalAmount,postsPerPage, paginate}) => {
       <ul class="inline-flex -space-x-px text-sm">
         { pageArray.map( number => (
             <li key={number}>
-            <button onClick={() => paginate(number)} class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-500"><a href="#">{number}</a></button>
+            <button onClick={() => {paginate(number); scrollTop()}} class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-500">{number}</button>
           </li>
         ))}
       
