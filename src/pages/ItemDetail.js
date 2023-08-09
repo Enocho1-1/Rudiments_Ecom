73251b7214f73e5edd20682a46f55cc4a7592c21
Enocho1-1 <enoch.okyere@gmail.com>
2023-08-09 14:16:57 -0400

    updates check item

--- src/pages/ItemDetail.js
+++ src/pages/ItemDetail.js
@@ -6,7 +6,10 @@ import "./ItemDetail.css"
 export const ItemDetail = () => {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(true)
+  const [size, setSize] = useState(false)
+  const [hidden, setHidden] = useState(false)
   const param = useParams()
+
   useEffect(() => {
     const fetchProducts = async () => {
       try{
@@ -23,6 +26,22 @@ export const ItemDetail = () => {
   },[param.id])
 
  const { title , price, imageUrl, imageUrl_Two, imageUrl_Three, imageUrl_Four} = data
+ let stringArray
+ const productitle = (string) => {
+   stringArray = string.split(' ')
+ }
+
+
+  useEffect(() => { 
+    if(title){
+    productitle(title)
+    stringArray.includes("Tee") || stringArray.includes( "Button") || stringArray.includes("Shirt") ? setSize(true) : setSize(false)
+  } else {
+    console.log('no title')
+  }
+  console.log(size)
+  })
+
 
  const mobileView = "flex flex-col"
   return (
@@ -31,7 +50,7 @@ export const ItemDetail = () => {
       <aside className={window.innerWidth < 769 ? mobileView : "flex flex-row justify-evenly"}>
         {window.innerWidth < 769 
         ? 
-        // Mobile View
+        // Responsive Design range:0px - 769px
         (
           <> 
           <div id="carouselExample" className="carousel slide">
@@ -72,17 +91,78 @@ export const ItemDetail = () => {
       : 
       (
         <>
-            <div className="grid grid-cols-2 grid-rows-2 tablet:max-laptop:grid-cols-gridCols tablet:max-laptop:grid-rows-gridRows w-full mt-4 individualImg">
+            {/* Product Images */}
+            <div className="grid grid-cols-2 grid-rows-2 tablet:max-laptop:grid-cols-gridCols tablet:max-laptop:grid-rows-gridRows tablet:max-laptop:w-[50%] individualImg">
               <img src={imageUrl} className=" w-full " alt="..."/>
               <img src={imageUrl_Two} className="w-full " alt="..."/>
               <img src={imageUrl_Three} className=" w-full " alt="..."/>
               {imageUrl_Four ? ( <img src={imageUrl_Four} className="block w-full " alt="..."/>):(<div className="bg-white w-fit h-fit"></div>)}
             </div>
-            <div className="flex tablet:max-laptop:w-[75%] laptop:w-[30%]">
-              <aside className="mt-12 tablet:desktop:ml-8">
-                <h1 className=" font-Bebas text-3xl">{title}</h1>
-                <p className="font-semibold text-2xl">{price}</p>
+
+            {/* Product Info */}
+            <div className="flex flex-col tablet:max-laptop:w-[50%] laptop:w-[30%]">
+              <aside className="mt-12 px-6">
+                <h1 className=" font-Inconsolata text-3xl">{title}</h1>
+                <p className="font-normal mt-2 text-2xl">{price}</p>
               </aside>
+
+              {/* Sizes */}
+              { size 
+              ? 
+              ( 
+              <aside className="my-4 flex justify-center">
+                <ul class="flex flex-row  text-sm">
+                    <li>
+                      <button className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">XXS</button>
+                    </li>
+                    <li>
+                      <button className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">XS</button>
+                    </li>
+                    <li>
+                      <button className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">S</button>
+                    </li>
+                    <li>
+                      <button className="fleX items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">M</button>
+                    </li>
+                    <li>
+                      <button className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">L</button>
+                    </li>
+                    <li>
+                      <button className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">XXL</button>
+                    </li>
+                    <li>
+                      <button className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">XXXL</button>
+                    </li>
+                </ul>
+              </aside>
+            ) 
+              :
+              (
+               <aside className="">
+                  <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" class="text-black bg-white hover:bg-slate-800 border border-slate-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">Select Size <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                      </svg></button>
+                   {/* Dropdwon */}
+                    <div id="dropdownHover" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                        <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownHoverButton">
+                          <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                          </li>
+                          <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                          </li>
+                          <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                          </li>
+                          <li>
+                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                          </li>
+                        </ul>
+                    </div>
+               </aside> 
+              )
+              }
+             
               <hr />
             </div>
         </>
