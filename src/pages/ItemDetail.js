df1c6eda75a51044fdb2f70a2fcac72d04397174
Enocho1-1 <73598581+Enocho1-1@users.noreply.github.com>
2023-08-10 03:58:16 -0400

    updates

--- src/pages/ItemDetail.js
+++ src/pages/ItemDetail.js
@@ -176,12 +176,13 @@ export const ItemDetail = () => {
                   </aside>
 
                   ): shoes ? 
-                  (  <aside className="">
+                  (  
+                  <aside className="">
                     <button onClick={() => setHidden(!hidden)} id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" className="text-black bg-white hover:bg-slate-800 border border-slate-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">Select Size <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
                         </svg></button>
                     {/* Dropdwon */}
-                      <div id="dropdownHover" className={ hidden ? "z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700" : "z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 h-[150px]"}>
+                      <div id="dropdownHover" className={ hidden ? "z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700" : "z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 h-[150px] overflow-y-scroll"}>
                           <ul className="py-2 text-sm text-gray-700" aria-labelledby="dropdownHoverButton">
                             <li>
                               <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">7</a>
@@ -206,11 +207,13 @@ export const ItemDetail = () => {
                             </li>
                           </ul>
                       </div>
-                </aside>
+                  </aside>
               ) : (
               <div></div>
               )
               }
+                    {/* Add To Cart */}
+                    <button type="button" className="flex justify-center self-center focus:outline-none text-black font-Bebas text-xl bg-yellow-400  focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg w-[75%] p-2 mt-2 ">Add To Bag<img src={Logo} className="h-6 mx-2"/></button>
             </aside>
           </div>
           </>
@@ -320,7 +323,7 @@ export const ItemDetail = () => {
                       <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
                     </svg></button>
                 {/* Dropdwon */}
-                  <div id="dropdownHover" className={ hidden ? "z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700" : "z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 h-[150px]"}>
+                  <div id="dropdownHover" className={ hidden ? "z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700" : "z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 h-[150px] overflow-y-scroll"}>
                       <ul className="py-2 text-sm text-gray-700" aria-labelledby="dropdownHoverButton">
                         <li>
                           <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">7</a>
