0984f368031de5931d506abf81264aea85cd68f0
Enocho1-1 <enoch.okyere@gmail.com>
2023-08-13 22:15:56 -0400

    local storage add

--- src/store/CartSlice.js
+++ src/store/CartSlice.js
@@ -1,15 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
+function AddtolocalStorage(cart, total){
+    localStorage.setItem('cart', JSON.stringify(cart))
+    localStorage.setItem('total', JSON.stringify(total))
+}
 const CartSlice = createSlice({
     name: "cart",
     initialState:{
-        cartItems:[],
-        total:0
+        cartItems: JSON.parse(localStorage.getItem('cart')) || [],
+        total: JSON.parse(localStorage.getItem('total')) || 0
     },
     reducers :{
         addItemToCart(state,action){
             const updateCart = state.cartItems.concat(action.payload)
             const updateTotal = state.total + action.payload.price 
+            AddtolocalStorage(updateCart, updateTotal)
             return {...state, cartItems:updateCart, total:updateTotal}
         }
     }
