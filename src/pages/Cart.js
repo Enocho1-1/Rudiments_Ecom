1eda6700be1dad05a80f18fd88c24dbca92b1fd9
Enocho1-1 <enoch.okyere@gmail.com>
2023-08-13 23:47:59 -0400

    cart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,8 @@
 import { useTitle } from "../hooks"
 import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
 import empty from "../assests/empty-cart.png"
+import arrow from "../assests/arrow.png"
 
 export const Cart = ({title}) => {
   useTitle(title)
@@ -8,14 +10,23 @@ export const Cart = ({title}) => {
   const total = useSelector(state => state.cart.total)
 
   return (
-    <section className="">
+    <section>
       {cart.length == 0 ?
-        <aside className="mt-12 flex flex-col justify-center items-center">
-          <img src={empty} className="h-12 w-[75px]" alt="empty" />
-         <h1>Bag empty</h1> 
+        <aside className="mt-24 flex flex-col  items-center">
+          <img src={empty} className="h-12 w-auto" alt="empty" />
+          <h1 className="mt-4 text-2xl font-Inconsolata font-medium">YOUR BAG IS EMPTY</h1> 
+          <Link to={"/"} className="text-xl font-Inconsolata mt-4 underline underline-offset-1">
+            Continue Shopping
+          </Link>
         </aside>:
-        <aside className="mt-12">
-          <h1>Bag occupy</h1>
+        <aside className=" m-auto max-w-7xl mt-24 flex flex-col">
+         <Link to={"/"} className="mt-4 px-4 flex">
+            <img src={arrow} className="h-6" alt="" />
+            <h1 className="text-xl font-Inconsolata font-light mx-2 ">Continue Shopping</h1>
+          </Link>
+          <div className="mt-12 self-center">
+            <h1 className="text-4xl font-Inconsolata font-semibold">MY SHOPPING BAG</h1>
+          </div>
         </aside>
       }
     </section>
