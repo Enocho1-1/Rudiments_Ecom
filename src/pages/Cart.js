01f535655535a729200874e6791c74dea9bb24fc
Enocho1-1 <73598581+Enocho1-1@users.noreply.github.com>
2023-08-16 02:31:59 -0400

    grid layout update

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -36,32 +36,39 @@ export const Cart = ({name}) => {
             {/* Cart Items */}
             <div className="mt-8">
               {
+              
                 cart.map( item => (
-                  <span className="flex flex-row mt-2 py-4">
-                    <img src={item.image} className="h-24 w-24" alt="product image" />
-                    <div className="whitespace-normal w-[250px] mx-4">
-                      <h1 className="text-2xl font-Inconsolata">{item.title}</h1>
-                    </div>
-                    <div className="mx-4 relative flex justify-center w-[150px]">
-                      <h1 className="text-2xl text-center font-Inconsolata absolute top-0">Size</h1>
-                      <p className="text-2xl font-Inconsolata self-center">{item.size}</p>
-                    </div>
-                    <div className="mx-4 relative flex justify-center w-[150px]">
-                      <h1 className="text-2xl text-center font-Inconsolata absolute top-0">Quantity</h1>
-                      <p className="text-2xl font-Inconsolata self-center">{item.quantity}</p>
-                    </div>
-                    <div className="mx-4 relative flex justify-center w-[150px]">
-                      <h1 className="text-2xl text-center font-Inconsolata absolute top-0">Price</h1>
-                      <p className="text-2xl font-Inconsolata self-center">${item.price}.00</p>
-                    </div>
-                    {/* Delete Item Button */}
-                    <svg onClick={() => {dispatch(removeItemCart(item))}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg hover:cursor-pointer self-center" viewBox="0 0 16 16">
-                      <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-                    </svg>
-                  </span> 
+                  <>
+                    <span className="grid grid-cols-6 mt-2 p-4">
+                      <Link to={`/${item.id}`}>
+                         <img src={item.image} className="h-24 w-24" alt="product image" />
+                      </Link>
+       
+                      <div className="whitespace-normal w-auto mx-4">
+                        <h1 className="text-2xl font-Inconsolata">{item.title}</h1>
+                      </div>
+                      <div className="mx-4 relative flex justify-center w-auto">
+                        <h1 className="text-2xl text-center font-Inconsolata absolute top-0">Size</h1>
+                        <p className="text-2xl font-Inconsolata self-center">{item.size}</p>
+                      </div>
+                      <div className="mx-4 relative flex justify-center w-auto">
+                        <h1 className="text-2xl text-center font-Inconsolata absolute top-0">Quantity</h1>
+                        <p className="text-2xl font-Inconsolata self-center">{item.quantity}</p>
+                      </div>
+                      <div className="mx-4 relative flex justify-center w-auto">
+                        <h1 className="text-2xl text-center font-Inconsolata absolute top-0">Price</h1>
+                        <p className="text-2xl font-Inconsolata self-center">${item.price}.00</p>
+                      </div>
+                      {/* Delete Item Button */}
+                      <svg onClick={() => {dispatch(removeItemCart(item))}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg hover:cursor-pointer self-center" viewBox="0 0 16 16">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                      </svg>
+                    </span> 
+                    <hr />
+                  </>
+                 
                 ))
               }
-              <hr />
               {/* Subtotal & Total */}
               <div className="mt-8">
                 <aside>
