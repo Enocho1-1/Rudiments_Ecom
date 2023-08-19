94c0f70b54a52b61d96154b36a0f52431277cd22
Enocho1-1 <73598581+Enocho1-1@users.noreply.github.com>
2023-08-19 13:55:35 -0400

    pagination updates

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -16,7 +16,7 @@ export const Pagination = ({totalAmount,postsPerPage, paginate}) => {
     <nav aria-label="Page navigation example" className="mt-6 flex justify-center">
       <ul class="inline-flex -space-x-px text-sm">
         { pageArray.map( number => (
-            <li key={number}>
+            <li key={number} onClick={() => scrollTop()}>
               <button onClick={() => {paginate(number); scrollTop()}} class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-500">{number}</button>
             </li>
         ))}
