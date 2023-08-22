9c987f33ab4bad5bd834f58778e590c97be5e931
Enocho1-1 <enoch.okyere@gmail.com>
2023-08-22 13:18:28 -0400

    cart link updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ export const Header = () => {
             <span className="self-center text-3xl leading-loose font-semibold whitespace-nowrap text-blk font-Bebas ">RUDIMENTS</span>
         </Link>
       <div className="flex md:order-2">
-        <button onClick={() => setIsHidden(!isHidden)} type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500  hover:bg-gray-300 rounded-lg text-sm p-2.5 mr-1" >
+        <button onClick={() => setIsHidden(!isHidden)} type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500  hover:text-slate-500 rounded-lg text-sm p-2.5 mr-1" >
           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
             <path stroke="currentColor"  strokeLinecap="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
           </svg>
@@ -35,7 +35,7 @@ export const Header = () => {
             <input type="text" name="item" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:border-slate-500" placeholder="Search Clothing Piece..."/>
           </form>
         </div>
-        <button data-collapse-toggle="navbar-search" onClick={() => setIsHidden(!isHidden)} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-search" aria-expanded="false">
+        <button data-collapse-toggle="navbar-search" onClick={() => setIsHidden(!isHidden)} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:text-slate-500 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-search" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor"  strokeLinecap="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
@@ -50,13 +50,17 @@ export const Header = () => {
           </div>
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium font-Bebas bg-white md:flex-row md:space-x-8 md:mt-0 md:border-0">
             <li>
-                <NavLink to="/shop" className="block py-2 pl-3 pr-4 text-xl font-Bebas text-blk rounded hover:bg-gray-100">Shop</NavLink>
+                <NavLink to="/shop" className="block py-2 pl-3 pr-4 text-xl font-Bebas text-blk rounded hover:text-slate-500">Shop</NavLink>
             </li>
             <li>
-                <NavLink to="/about" className="block py-2 pl-3 pr-4 text-xl font-Bebas text-blk rounded hover:bg-gray-100">About</NavLink>
+                <NavLink to="/about" className="block py-2 pl-3 pr-4 text-xl font-Bebas text-blk rounded hover:text-slate-500">About</NavLink>
             </li>
             <li>
-                <NavLink to="/cart" className="block py-2 pl-3 pr-4 text-xl font-Bebas text-blk rounded hover:bg-gray-100">Cart ( {cart.length} )</NavLink>
+                <NavLink to="/cart" className=" pl-3 pr-4 text-xl font-Bebas text-blk rounded hover:text-slate-500">
+                <span className="text-4xl bi bi-cart-fill relative">
+                    <span className="text-white text-sm absolute -top-1 left-3.5 bg-rose-500 px-2 rounded-full ">{cart.length}</span>
+                  </span> 
+                </NavLink>
             </li>
           </ul>
         </div>
--- src/pages/Filter.js
+++
@@ -1,7 +0,0 @@
-
-
-export const Filter = () => {
-  return (
-    <div>Filter</div>
-  )
-}
