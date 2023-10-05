080d774eb1003a4156981b8a65e18321819b979f
Enocho1-1 <enoch.okyere@gmail.com>
2023-10-05 03:02:06 -0400

    npm i react-query

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider} from "react-query"
 import { Provider } from 'react-redux';
 import { FilterProvider } from './context/filterContext';
 import { store } from './store/Store';
@@ -9,17 +10,21 @@ import './index.css';
 import App from './App';
 
 
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-      <FilterProvider >
-        <ScrollTop />
-        <App />
-      </FilterProvider>
-      </BrowserRouter> 
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <BrowserRouter>
+        <FilterProvider >
+          <ScrollTop />
+          <App />
+        </FilterProvider>
+        </BrowserRouter> 
+      </Provider>
+    </QueryClientProvider>
+    
   </React.StrictMode>
 );
 
