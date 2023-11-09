f558a18fc205a77299fa4c05a5e8ddb6ee76e279
Enocho1-1 <enoch.okyere@gmail.com>
2023-11-09 10:13:43 -0500

    payment updates

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,10 +1,10 @@
-
+import{ MyBag } from "../Delivery/components/MyBag"
 
 export const Payment = () => {
   return (
     <section>
         <div className="mt-[4.688rem] flex justify-center">
-            payment
+            <MyBag/>
         </div>
       
     </section>
