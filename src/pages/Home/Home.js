2b5f802ed724ba4e7f5a318c9db6d725753a95c9
Enocho1-1 <73598581+Enocho1-1@users.noreply.github.com>
2023-11-17 22:50:22 -0500

    promo updates

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,6 +7,7 @@ import { NavLink } from "react-router-dom"
 export const Home = ({title}) => {
 
   useTitle(title)
+  const [promoModal,setPromodal] = useState(true)
   const pieceButtons = [
     {
       name:"T-Shirts",
@@ -42,7 +43,7 @@ export const Home = ({title}) => {
   const newUser = JSON.parse(sessionStorage.getItem("newUser"))
   return (
     <section className="relative">
-      { newUser  &&  <PromoModal/>}
+      { newUser && promoModal  &&  <PromoModal setPromodal={setPromodal}/>}
       <HomeCarousel/>
       {/* Collection Buttons */}
       <aside className="w-full grid max-mobile:grid-cols-2 mobile:max-tablet:grid-cols-2 tablet:grid-cols-3">
